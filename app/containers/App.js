d8522c5be2823517a9bd53683f57de60adbca69c
Martin Bramwell <martinhbramwell@gmail.com>
2017-12-27 07:49:48 -0500

    Bug (master): Revert master to start. All work now in dev.
    Signed-off-by: Martin Bramwell <martinhbramwell@gmail.com>

--- app/containers/App.js
+++ app/containers/App.js
@@ -2,8 +2,6 @@
 import React, { Component } from 'react';
 import type { Children } from 'react';
 
-import { Link } from 'react-router-dom';
-
 export default class App extends Component {
   props: {
     children: Children
@@ -12,50 +10,8 @@ export default class App extends Component {
   render() {
     return (
       <div>
-
-        <nav className="navbar navbar-inverse visible-xs">
-          <div className="container-fluid">
-            <div className="navbar-header">
-              <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-              </button>
-              <a className="navbar-brand" href="#">Logo</a>
-            </div>
-            <div className="collapse navbar-collapse" id="myNavbar">
-              <ul className="nav navbar-nav">
-                <li className="active"><a href="#">Dashboard</a></li>
-                <li><a href="#">Age</a></li>
-                <li><a href="#">Gender</a></li>
-                <li><a href="#">Geo</a></li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-
-        <div className="container-fluid">
-          <div className="row content">
-            <div className="col-sm-3 sidenav hidden-xs">
-              <br/>
-              <h1>Iridium Blue</h1>
-              <h2>Facturacion Electronica</h2>
-              <ul className="nav nav-pills nav-stacked">
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/load">File Import</Link></li>
-                <li><Link to="/counter">Counter</Link></li>
-              </ul>
-            </div>
-
-            <div className="clearfix visible-xs"></div>
-            <div className="col-sm-9">
-              {this.props.children}
-            </div>
-          </div>
-        </div>
-
+        {this.props.children}
       </div>
-
     );
   }
 }
--- app/routes.js
+++ app/routes.js
@@ -2,14 +2,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
 import App from './containers/App';
-import FileImportPage from './containers/FileImportPage';
 import HomePage from './containers/HomePage';
 import CounterPage from './containers/CounterPage';
 
 export default () => (
   <App>
     <Switch>
-      <Route path="/load" component={FileImportPage} />
       <Route path="/counter" component={CounterPage} />
       <Route path="/" component={HomePage} />
     </Switch>
