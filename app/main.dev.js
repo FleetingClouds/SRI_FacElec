9afb474d22af2c25971a40cef951c398ff65169f
Martin H. Bramwell <martinhbramwell@gmail.com>
2018-01-07 16:05:34 -0500

    Bug (all): Minor corrections
    Signed-off-by: Martin H. Bramwell <martinhbramwell@gmail.com>

--- app/main.dev.js
+++ app/main.dev.js
@@ -24,8 +24,6 @@ if ( process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true'
   require( 'electron-debug' )();
   const path = require( 'path' );
   const p = path.join( __dirname, '..', 'app', 'node_modules' );
-  console.log( '==================' );
-  console.log( p );
   require( 'module' ).globalPaths.push( p );
 }
 
